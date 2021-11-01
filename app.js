27b39cf5b7644502c09de3909d4a438545245ee5
shenoragn <shenoranglin95@gmail.com>
2021-11-01 00:19:30 -0500

    incomplete Ex3

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var btnSEARCH;
 var bxSEARCH;
 var rsltSection;
+var superheroes = <?php echo json_encode($superheroes); ?>
 
 window.onload = function() {
-    btnSEARCH = document.querySelector("button");
+    btnSEARCH = document.querySelector(".btn");
     btnSEARCH.addEventListener("click", BtnHandler);
     bxSEARCH = document.getElementById("hero");
     rsltSection = document.querySelector(".results");
@@ -11,13 +12,25 @@ window.onload = function() {
 
 function BtnHandler(e){
     e.preventDefault();
-    fetch("superheroes.php", {
-        method: 'POST',
-        body: bxSEARCH.value.trim(),
-        headers: {
-            'Content-Type': 'text/plain'
-        }
-   })
-   .then(response => response.text())
-   .then(data => rsltSection.innerHTML = data);
+	if (bxSEARCH==null){
+		fetch("superheroes.php", {
+			method: 'POST',
+			body: bxSEARCH.value.trim(),
+			headers: {
+				'Content-Type': 'text/plain'
+			}
+	   })
+	   .then(response => response.text())
+	   .then(data => rsltSection.innerHTML = data);
+	}else if(<?php in_array(bxSEARCH.value.trim(), $superheroes) ?>){
+		fetch ( "superheroes.php", {
+			method: 'POST',
+			body: bxSEARCH.value.trim(),
+			headers: {
+				'Content-Type': 'text/plain'
+			}
+	   }).then(data = rsltSection.innerHTML = superheroes[btnSEARCH.value.trim()];)
+	}else {
+		rsltSection.setAttribute("class", "notfound").innerHTML = "SUPERHERO NOT FOUND";
+	}
 }
